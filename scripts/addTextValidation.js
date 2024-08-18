010eb33d9feecff4d112465795d53388a621a8a5
Sebastien Olhagaray <sebastien.olha@gmail.com>
2024-08-18 23:24:44 +0200

    add first letter capital text validation

--- scripts/addTextValidation.js
+++ scripts/addTextValidation.js
@@ -2,7 +2,7 @@ const addTextValidation = (validationLevel, textScoreLanding) => {
     const textValidation = document.createElement('div');
     textValidation.classList.add(`text-validation--${validationLevel}`);
     textValidation.style.animation = 'score-text 700ms ease-out forwards';
-    textValidation.textContent = validationLevel;
+    textValidation.textContent = validationLevel.charAt(0).toUpperCase() + validationLevel.slice(1);;
     textScoreLanding.replaceChildren(textValidation);
     
     setTimeout(() => {
